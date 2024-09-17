cc6bab033c73e000117cfe3c17a30db32e667030
camila chauca <camilajasmin.ec@gmail.com>
2024-09-16 22:22:46 -0300

    Exercicio 5

--- exerciciosPy/numero5.py
+++ exerciciosPy/numero5.py
@@ -5,3 +5,17 @@ IMPORTANTE:
 a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
 b) Evite usar funções prontas, como, por exemplo, reverse;
 '''
+print('Invertendo os caracteres de uma string!')
+print(40*'-')
+
+string = input('Digite seu nome: ')
+tamanhoString = len(string)
+i = 0
+
+
+print(f'A string é "{string}" e seu tamanho é de {tamanhoString} caracteres incluindo os espaços')
+
+while i > -tamanhoString:
+    i-=1
+    char = string[i]
+    print(char)
